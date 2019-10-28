04e7b105f89cde1fb843289d5041742cd6b135cf
Zahid <zahid.choudhury@gmail.com>
2019-10-28 12:45:17 +0200

    Updates

---
+++ precache-manifest.ac9c13bf8120c4b8a6eeb68abc1b5816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fde373c6381c7c90893073c9bd54a3",
+    "url": "/SPMS/index.html"
+  },
+  {
+    "revision": "306a79babbc2c6e6621d",
+    "url": "/SPMS/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "16c0026cfaf98482bfe2",
+    "url": "/SPMS/static/css/main.7eb662fc.chunk.css"
+  },
+  {
+    "revision": "306a79babbc2c6e6621d",
+    "url": "/SPMS/static/js/2.5c7dc489.chunk.js"
+  },
+  {
+    "revision": "16c0026cfaf98482bfe2",
+    "url": "/SPMS/static/js/main.1d14637b.chunk.js"
+  },
+  {
+    "revision": "48a281f8f05f09363e9f",
+    "url": "/SPMS/static/js/runtime-main.c82a67d1.js"
+  }
+]);
